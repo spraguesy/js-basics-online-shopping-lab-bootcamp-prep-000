b983a2de3e02c7f87c57f04378cd4276b97571d2
Andrew Sprague <andrewsprague@outlook.com>
2018-07-16 19:58:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,34 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ var currentItem = {};
+ currentItem.itemName = item;
+ currentItem.itemPrice = Math.floor(Math.random()*100);
+ cart.push(currentItem);
+ return `${currentItem.itemName} has been added to your cart.`
 }
 
 function viewCart() {
   // write your code here
+  var itemString;
+  var fullString = '';
+  if (cart.length > 0) {
+    for (var i = 0; i < cart.length; i++) {
+      
+      itemString = `${cart[i].itemName} at $${cart[i].itemPrice}`
+      if (i === (cart.length - 1) && i !==0) {
+        itemString += ", and "
+      } else if (i < cart.length -1) {
+        itemString += ", "
+      }
+      
+      fullString += itemString
+    }  
+    
+    return `In your cart, you have ${fullString}.`
+  } else {
+    return "Your shopping cart is empty."
+  }
 }
 
 function total() {
