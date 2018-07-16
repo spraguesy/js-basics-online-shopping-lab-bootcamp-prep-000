8fb0a0c5f151c100d1c6b2a05419ed10d8d90611
Andrew Sprague <andrewsprague@outlook.com>
2018-07-16 20:25:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,6 +56,11 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
+  for (var i =0; i < cart.length; i++) {
+    if (item === cart[i].itemString) {
+      delete cart[i];
+    }
+  }
 }
 
 function placeOrder(cardNumber) {
